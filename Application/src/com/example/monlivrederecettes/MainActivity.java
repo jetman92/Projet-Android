83bb01bd24c51e03bea30eb9109a3569936d74e8
Stitif <stitif.d@gmail.com>
2013-11-20 12:02:35 +0100

    2ème activity déployer

--- Application/src/com/example/monlivrederecettes/MainActivity.java
+++ Application/src/com/example/monlivrederecettes/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.monlivrederecettes;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
+	}
+	
+	/** Called when the user clicks the Send button */
+	//Lancement de l'acitivity via le boutton 1
+	public void mesRecettes(View view) {
+	    // Do something in response to button
+		Intent intent  = new Intent(this, MyRecettesActivity.class);
+		startActivity(intent);
 	}
 
 }
