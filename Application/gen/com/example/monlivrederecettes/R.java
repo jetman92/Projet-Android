b8f60951b0c308d64994a78d016ed672dd675621
Arnaud d'Artigues <arnaud.dartigues@gmail.com>
2013-11-20 12:02:26 +0100

    Suite du dev

--- Application/gen/com/example/monlivrederecettes/R.java
+++ Application/gen/com/example/monlivrederecettes/R.java
@@ -8,6 +8,9 @@
 package com.example.monlivrederecettes;
 
 public final class R {
+    public static final class array {
+        public static final int ItemsMenu=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,18 +27,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090004;
+        public static final int button1=0x7f090003;
+        public static final int button2=0x7f090001;
+        public static final int button3=0x7f090000;
+        public static final int button4=0x7f090002;
     }
     public static final class layout {
         public static final int activity_menu=0x7f030000;
+        public static final int activity_myrecettes=0x7f030001;
     }
     public static final class menu {
-        public static final int menu=0x7f070000;
+        public static final int menu=0x7f080000;
+        public static final int my_recettes=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int addRecettes=0x7f050006;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int lookRecettes=0x7f050004;
+        public static final int myRecettes=0x7f050003;
+        public static final int title_activity_my_recettes=0x7f050007;
+        public static final int toBuy=0x7f050005;
     }
     public static final class style {
         /** 
@@ -59,10 +73,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- Application/src/com/example/monlivrederecettes/MainActivity.java
+++ Application/src/com/example/monlivrederecettes/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.monlivrederecettes;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+	//private String[] MyMenu;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -16,7 +21,21 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu, menu);
+	
+		
+		
 		return true;
+				
+		
+		/*MyMenu = getResources().getStringArray(R.array.ItemsMenu);
+		ArrayAdapter myList = new ArrayAdapter(this, R.layout.activity_menu, );*/
 	}
+	
+	public void mesRecettes (View v){
+		Intent intent = new Intent(this, MyRecettesActivity.class);
+		startActivity(intent);
+	}
+
+	
 
 }
